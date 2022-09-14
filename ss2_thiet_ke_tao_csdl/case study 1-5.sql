247e0c73a284e27a157606ca1f96b70b5b943274
NguyenHoang272 <nguyenhoang29082001@gmail.com>
2022-09-14 16:59:19 +0700

    update

--- ss2_thiet_ke_tao_csdl/case study 1-5.sql
+++ ss2_thiet_ke_tao_csdl/case study 1-5.sql
@@ -1,2 +1,3 @@
 use furama_management;
 select ho_ten from nhan_vien where( ho_ten regexp '^[HTK]') and (char_length(ho_ten) <= 15);
+select * from khach_hang where (timestampdiff(year,ngay_sinh,curdate()) between 18 and 50) and (dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%');
