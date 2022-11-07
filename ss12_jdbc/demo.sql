63a37591f6f690b6f5ce6ee9668a684538993236
NguyenHoang272 <nguyenhoang29082001@gmail.com>
2022-11-07 15:41:37 +0700

    ss

---
+++ ss12_jdbc/demo.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS demo;
+create database demo;
+use demo;
+
+create table users(
+id int not null auto_increment,
+`name` varchar(50),
+email varchar(50),
+country varchar(50),
+primary key(id)
+);
+
+insert into users(name, email, country) values('Minh','minh@codegym.vn','Viet Nam');
+insert into users(name, email, country) values('Kante','kante@che.uk','Kenia');
---
+++ ss12_jdbc/exercise/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dell
+  Date: 11/7/2022
+  Time: 1:50 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<c:redirect url="/user"></c:redirect>
